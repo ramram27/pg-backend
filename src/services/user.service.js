5e80b35739f72205523e04a14f79aa4445da9270
Rohit <rohitkumar@caplineservices.com>
2025-10-07 14:28:16 +0530

    jwt remove

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,20 +1,14 @@
 const User = require('../model/User.model')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
+
 require('dotenv').config()
 
 const screateKey = process.env.JWT_SECRET
 const createUser = async (name, email, password) => {
     try {
-        const roundSalt = 8;
-        const hashPassword = await bcrypt.hash(password, roundSalt)
-        const user = await User.create(name, email, hashPassword);
-        const payload = { id: user.id, email: user.email }
-        const token = await jwt.sign(payload, screateKey, { expiresIn: '1h' })
+        const user = await User.create(name, email, password);
         return {
             message: 'User registered successfully',
-            user,
-            token
+            user
         };
     } catch (err) {
         throw err;
